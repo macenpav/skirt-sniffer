11446339db099c42f9aa23a53cc4384065ce0f02
Pavel Macenauer <macenauer.p@gmail.com>
2019-05-27 22:54:18 +0200

    Added initial date check when script gets called for the first time during that day

--- db_module.py
+++ db_module.py
@@ -1,12 +1,20 @@
 import sqlite3
 from os import path
+from datetime import datetime
 DIR_PATH = path.dirname(path.realpath(__file__))
 
 database_name = r"blackmountain.db"
 
+default_today_table = r"today_entry"
+default_print_table = r"prints"
+default_product_table = r"products"
+
 sql_create_product_table = r"CREATE TABLE IF NOT EXISTS {0} (id INTEGER PRIMARY KEY, product_name text, url text)"
+sql_create_today_table = r"CREATE TABLE IF NOT EXISTS {0} (date text PRIMARY KEY)"
 sql_select_product = r"SELECT 1 FROM {0} WHERE id=?"
 sql_insert_product = r"INSERT INTO {0} VALUES (?, ?, ?)"
+sql_today_entry_exists = r"SELECT 1 FROM {0} WHERE date=?"
+sql_insert_today = r"INSERT INTO {0} VALUES (?)"
 
 
 def open_connection():
@@ -31,7 +39,7 @@ def insert_product(connection, table_name, pid, name, url):
     c.execute(sql_select_product.format(table_name), (pid,))
     exists = c.fetchone()
     # TODO: we may move this select to SQL
-    if not exists:
+    if exists is None:
         c.execute(sql_insert_product.format(table_name), (pid, name, url))
         connection.commit()
         return True
@@ -41,5 +49,29 @@ def insert_product(connection, table_name, pid, name, url):
 def create_database(connection, table_name):
     c = connection.cursor()
     c.execute(sql_create_product_table.format(table_name))
+    connection.commit()
+
+
+def create_today_database(connection):
+    c = connection.cursor()
+    c.execute(sql_create_today_table.format(default_today_table))
+    connection.commit()
+
+
+def is_today_init(connection, table_name=default_today_table):
+    c = connection.cursor()
+    today = datetime.strftime(datetime.today(), '%y-%m-%d')
+    c.execute(sql_today_entry_exists.format(table_name), (today,))
+    exists = c.fetchone()
+    if exists is None:
+        return True
+    else:
+        return False
+
+
+def insert_today(connection, table_name=default_today_table):
+    c = connection.cursor()
+    today = datetime.strftime(datetime.today(), '%y-%m-%d')
+    c.execute(sql_insert_today.format(table_name), (today,))
     connection.commit()
 
--- skirt-sniffer.py
+++ skirt-sniffer.py
@@ -4,11 +4,13 @@ import db_module
 from jinja2 import Environment, FileSystemLoader
 from os import path
 import time
+from datetime import datetime
 
 DIR_PATH = path.dirname(path.realpath(__file__))
 
 NEW_SKIRT_SUBJECT = r"Novinka! - {0}"
 NEW_PRINT_SUBJECT = r"NOVÉ TISKY! - {0}"
+DAY_INIT_SUBJECT = r"Keporkak se probouzí! - {0}"
 PAGE_LIMIT = 20   # limit to read pages so we don't deadlock while mining data
 
 
@@ -27,13 +29,24 @@ def send_email(subject_base, data):
     email.send()
 
 
-def run_base():
-    table_name = r"products"
+def send_wakeup_email():
+    email = email_module.Email()
+    subject = DAY_INIT_SUBJECT.format(datetime.strftime(datetime.today(), '%y-%m-%d'));
 
+    file_loader = FileSystemLoader(path.join(DIR_PATH, 'templates'))
+    env = Environment(loader=file_loader)
+    template = env.get_template('wakeup.html')
+
+    email.set_content(template.render(title=subject))
+    email.set_subject(subject)
+    email.send()
+
+
+def run_base():
     page_no = 0
 
     conn = db_module.open_connection()
-    db_module.create_database(conn, table_name)
+    db_module.create_database(conn, db_module.default_product_table)
     new_data = []
 
     while True:
@@ -42,7 +55,7 @@ def run_base():
 
         # mine until we can, we don't know how many pages are in the e-shop
         if products and (page_no < PAGE_LIMIT):
-            new_products = db_module.insert_products(conn, table_name, products)
+            new_products = db_module.insert_products(conn, db_module.default_product_table, products)
             if new_products:
                 for prod in new_products:
                     new_data.append(prod)
@@ -63,24 +76,34 @@ def run_base():
 
 
 def run_print():
-    table_name = "prints"
-
     conn = db_module.open_connection()
-    db_module.create_database(conn, table_name)
+    db_module.create_database(conn, db_module.default_print_table)
 
     url = data_module.get_url_print()
     products = data_module.mine(url)
 
     new_data = None
     if products:
-        new_data = db_module.insert_products(conn, table_name, products)
+        new_data = db_module.insert_products(conn, db_module.default_print_table, products)
     db_module.close_connection(conn)
 
-    if new_data is not None:
+    if new_data:
         print("run_print({0}): New data found - sending email.".format(str(time.time())))
         send_email(NEW_PRINT_SUBJECT, new_data)
     else:
-        print("run_print({0}): No new data.")
+        print("run_print({0}): No new data.".format(str(time.time())))
+
+
+def day_init_check():
+    conn = db_module.open_connection()
+    db_module.create_today_database(conn)
+
+    if db_module.is_today_init(conn):
+        db_module.insert_today(conn)
+        print("day_init_check({0}): Today's first mining - sending email.".format(str(time.time())))
+        send_wakeup_email()
+
+    db_module.close_connection(conn)
 
 
 if __name__ == "__main__":
@@ -88,6 +111,7 @@ if __name__ == "__main__":
     if data_module.is_server_available() is False:
         print("main({0}): Unable to ping server.".format(str(time.time())))
     else:
+        day_init_check()
         print("main({0}): Starting to mine data.".format(str(time.time())))
         run_base()
         run_print()
